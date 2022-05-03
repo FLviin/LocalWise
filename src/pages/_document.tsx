5f2b4cc08e52ccbdff8ed910278a7ba85cd475b3
Flávio Oliveira <83542246+FLviin@users.noreply.github.com>
2022-05-03 11:26:17 -0300

    ..

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, {Html, Head, Main, NextScript} from "next/document";
+
+export default class myDocument extends Document {
+    render() {
+        return(
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Oswald&family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
+                    <title>LocalWise</title>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
