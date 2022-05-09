e1fe6f361ff6d47eea6f676418dfe4bb91141e18
Flávio Oliveira <83542246+FLviin@users.noreply.github.com>
2022-05-09 13:21:17 -0300

    att

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,13 @@
+import { FormControl, InputGroup } from 'react-bootstrap';
+import { FaSearch } from 'react-icons/fa';
 import styles from './styles.module.scss';
 
 interface HeaderProps{
     onOpenSingInModal: () => void;
 }
+const myLoader = ({ src, width, quality }) => {
+    return `../../../public/images/search.svg/${src}?w=${width}&q=${quality || 75}`
+  }
 
 export function Header({onOpenSingInModal} : HeaderProps){
 
@@ -12,7 +17,14 @@ export function Header({onOpenSingInModal} : HeaderProps){
                 <img
                 src="/images/logo.png" alt="logo" 
                 />
-                
+                <InputGroup className="mb-3">
+                    <FormControl
+                        placeholder="Recipient's username"
+                        aria-label="Recipient's username"
+                        aria-describedby="basic-addon2"
+                    />
+                    <InputGroup.Text id="basic-addon2"><FaSearch/></InputGroup.Text>
+                </InputGroup>
                 <button type="button" className={styles.signInButton} onClick={onOpenSingInModal}>
                     <span>Sing In</span>
                 </button>
